8cd5b785571731d8d1d019343ee3f1e07693d543
John D. Ament <john.ament@spartasystems.com>
2014-08-30 09:49:45 -0400

    Added deck.js

---
+++ slides/src/site/deckjs/test/settings.js
@@ -0,0 +1,7 @@
+/*******************************************************************************
+ * Copyright (c) 2013 - 2014 Sparta Systems, Inc.
+ ******************************************************************************/
+
+// SETTINGS, VARS, UTILITY FUNCTIONS
+jasmine.getFixtures().fixturesPath = 'fixtures';
+var defaults = $.deck.defaults;
